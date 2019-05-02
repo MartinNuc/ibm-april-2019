b370e61efec933178c19b5417aa66b97139e6c1c
Martin Nuc <martin@nuc.cz>
2019-05-02 16:39:22 +0100

    primes

--- angular/hello-world/src/app/app.component.ts
+++ angular/hello-world/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 import { Article } from './article';
-import { VirtualTimeScheduler, Observable } from 'rxjs';
+import { VirtualTimeScheduler, Observable, from, interval } from 'rxjs';
+import { distinctUntilChanged, take, skip, filter } from 'rxjs/operators';
 import { ArticlesService } from './articles.service';
 
 @Component({
@@ -23,37 +24,20 @@ export class AppComponent implements AfterViewInit {
   counterButton: ElementRef<HTMLButtonElement>;
 
   constructor(public articlesService: ArticlesService) {
+    interval(200).pipe(
+      filter(x => isPrime(x)),
+      skip(5),
+      take(4)
+    ).subscribe(x => console.log(x));
 
-    resolveAfterTime(2000)
-      .then(() => console.log('after 2 seconds'));
-
-    const obs$ = interval(2000);
-    obs$.subscribe(num => {
-      this.num = num;
-      console.log(num);
-    });
-
-    function resolveAfterTime(ms) {
-      return new Promise(resolve => {
-        setTimeout(() => {
-          resolve();
-        }, ms);
-      });
+    function isPrime(n) {
+      for (let i = 2; i < n; i++) {
+        if (n % i === 0) {
+          return false;
+        }
+      }
+      return n !== 1 && n !== 0;
     }
-
-    function interval(ms) {
-      return new Observable<number>(observer => {
-        let x = 0;
-        setInterval(() => {
-          observer.next(x);
-          x++;
-        }, ms);
-      });
-    }
-
-
-
-
   }
 
   toggleColor() {
