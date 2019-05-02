a59a9cab71d4f8b76ae797310d07c4cb5ffc2042
Martin Nuc <martin@nuc.cz>
2019-05-02 13:47:36 +0100

    wip

--- angular/hello-world/src/app/app.component.spec.ts
+++ angular/hello-world/src/app/app.component.spec.ts
@@ -1,12 +1,23 @@
 import { TestBed, async, ComponentFixture } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { ArticlesService } from './articles.service';
+import { ArticleCounterComponent } from './article-counter/article-counter.component';
+import { ArticleComponent } from './article/article.component';
+import { WordsCounterPipe } from './words-counter.pipe';
+import { By } from '@angular/platform-browser';
 
-describe('AppComponent', () => {
+fdescribe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        ArticleCounterComponent,
+        ArticleComponent,
+        WordsCounterPipe
       ],
+      providers: [
+        ArticlesService
+      ]
     }).compileComponents();
   }));
 
@@ -19,5 +30,27 @@ describe('AppComponent', () => {
     fixture.detectChanges();
   });
 
+  it('should increment counter on button click', () => {
+    const button = fixture.debugElement.query(By.css('#counter-button'));
+    const counter = fixture.debugElement.query(By.css('#counter'));
+    expect(button.nativeElement.textContent).toBe('0');
+    expect(counter.nativeElement.textContent).toBe('0');
+    button.triggerEventHandler('click', null);
+    fixture.detectChanges();
+    expect(button.nativeElement.textContent).toBe('1');
+    expect(counter.nativeElement.textContent).toBe('1');
+  });
 
+  it('should show one more article after clicking create article button', () => {
+    const button = fixture.debugElement.query(By.css('#write-article'));
+    const countBeforeAdding = fixture.debugElement.queryAll(
+      By.directive(ArticleComponent)
+    ).length;
+    button.triggerEventHandler('click', null);
+    fixture.detectChanges();
+    const countAfterAdding = fixture.debugElement.queryAll(
+      By.directive(ArticleComponent)
+    ).length;
+    expect(countAfterAdding).toBe(countBeforeAdding + 1);
+  });
 });
